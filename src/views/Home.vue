2a6c7f015773f8269dcae0ca7ad4dab9afdf0301
Johan Romero <johan.romero5879@gmail.com>
2021-05-14 15:00:49 -0500

    Arreglo de contenedores

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,15 @@
 <template>
-  
   <div>
- <body>  
-<div class='contenedor'>
-  
-  
-<div class="grid-item">
-    <v-container fluid>
-      <v-row>
-        
-        <v-col
-          cols="12"
-          sm="6"
-          md="5"
-        >
+    <body>
+      <div class="contenedor">
+        <form class="form">
           <v-text-field
             label="Usuario"
             prepend-icon="mdi-account"
             filled
           ></v-text-field>
-        </v-col>
-      </v-row>
-    </v-container>
-    
-  </div>
-  <div class="grid-item2">
-    <v-container fluid>
-      <v-row>
-        <v-col
-          cols="12"
-          sm="20"
-          md="5"
-        >
           <v-text-field
-           
-            filled 
+            filled
             prepend-icon="mdi-account-key"
             :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
             :rules="[rules.required]"
@@ -43,63 +18,51 @@
             label="Contraseña"
             @click:append="show1 = !show1"
           ></v-text-field>
-        </v-col>
-
-         </v-row>
-    </v-container>
-    </div>
- </div>
- </body>
- </div>
-  
+          <v-btn color="primary">Click me</v-btn>
+        </form>
+      </div>
+    </body>
+  </div>
 </template>
 
 
 
 
 <style>
-body{
-
-}
-.contenedor { 
-width: 1349px;
-height: 570px;
-border: 5px solid blue;
-box-sizing: border-box;
-background:greenyellow ;
+.form {
+  background-color: white;
+  opacity: 1 !important;
+  width: 700px;
+  height: 500px;
+  padding: 24px;
 }
 
-.grid-item {
-  padding-top: 300px;
-  padding-right: 1px;
-  padding-bottom: 0px;
-  padding-left: 500px;
-  background: transparent ;
+.contenedor {
+  width: 1349px;
+  height: 570px;
+  border: 5px solid blue;
+  box-sizing: border-box;
+  background: url("/assets/night.jpg");
+  opacity: 0.5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-.grid-item2 {
-  
-  padding-top: 1px;
-  padding-right: 1px;
-  padding-bottom: 50px;
-  padding-left: 500px;
-  background:transparent ;
-}
+
 </style>
 
 <script>
-
-  export default {
-    
-     data () {
-      return {
-        show1: false,
-        password: 'Password',
-        rules: {
-          required: value => !!value || 'Ingrese su contraseña',
-          emailMatch: () => (`El usuario o la contraeña son incorrectos`),
-        },
-      }
-    },
-    name: 'Home'
-  }
+export default {
+  data() {
+    return {
+      show1: false,
+      password: "Password",
+      rules: {
+        required: (value) => !!value || "Ingrese su contraseña",
+        emailMatch: () => `El usuario o la contraeña son incorrectos`,
+      },
+    };
+  },
+  name: "Home",
+};
 </script>
